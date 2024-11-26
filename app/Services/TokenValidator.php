c6cb486a26f2e4724da87c2b290ba8f2f580f98e
Robert <robert.deabreu@borrox.com>
2024-11-26 03:14:21 +0100

    add method validate string

--- app/Services/TokenValidator.php
+++ app/Services/TokenValidator.php
@@ -4,5 +4,32 @@
 
     class TokenValidator
     {
+        /**
+         * Validates if a string containing parentheses and brackets.
+         *
+         * @param string $input The string to be validated.
+         * @return bool Returns true if the string is balanced, false if it is not.
+         */
+        public function validateFormat(string $input): bool
+        {
+            $stack = [];
+            $open = ['{', '(', '['];
+            $close = ['}' => '{', ')' => '(', ']' => '['];
 
+            foreach (str_split($input) as $char) {
+                if (in_array($char, $open)) {
+                    array_push($stack, $char);
+                }
+
+                if (isset($close[$char]) && end($stack) == $close[$char]) {
+                    array_pop($stack);
+                }
+
+                if (!in_array($char, $open) && !isset($close[$char])) {
+                    return false;
+                }
+            }
+
+            return empty($stack);
+        }
     }
