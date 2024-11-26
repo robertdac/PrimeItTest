1cba2ba2da3f45955145652d5ba8feaa27eb5787
Robert <robert.deabreu@borrox.com>
2024-11-26 02:49:15 +0100

    new class tokenValidator

---
+++ tests/Feature/TokenValidatorTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Services\TokenValidator;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TokenValidatorTest extends TestCase
+{
+    protected $tokenValidator;
+
+
+}
