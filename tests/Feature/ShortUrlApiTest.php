ae93e8209ee6efa091165dee96fedc6e7fbe2802
Robert <robert.deabreu@borrox.com>
2024-11-26 02:36:11 +0100

    add method it_returns_a_shortened_url

--- tests/Feature/ShortUrlApiTest.php
+++ tests/Feature/ShortUrlApiTest.php
@@ -11,4 +11,32 @@ class ShortUrlApiTest extends TestCase
 {
 const CUSTOM_TOKEN = '[]{}()';
 
+    /** @test */
+    public function it_returns_a_shortened_url()
+    {
+
+        $this->mockTinyUrlApi('http://www.example.com', 'https://tinyurl.com/example');
+
+        $response = $this->postJson('/api/v1/short-urls', [
+            'url' => 'http://www.example.com',
+        ], ['Authorization' => 'Bearer ' . self::CUSTOM_TOKEN]);
+
+        // Verificar que la respuesta tenga el status correcto
+        $response->assertStatus(200)
+            ->assertJson([
+                'url' => 'https://tinyurl.com/example',
+            ]);
+    }
+
+
+    protected function mockTinyUrlApi($url, $shortenedUrl)
+    {
+        Http::fake([
+            'https://api.tinyurl.com/create' => Http::response([
+                'result' => $shortenedUrl,
+            ], 200),
+        ]);
+    }
+
+
 }
