549d6bc5dcd06debb40da6da9cb1f243454d71a6
Robert <robert.deabreu@borrox.com>
2024-11-26 02:40:35 +0100

    add method it_returns_error_when_url_is_invalid

--- tests/Feature/ShortUrlApiTest.php
+++ tests/Feature/ShortUrlApiTest.php
@@ -1,51 +1,64 @@
 <?php
 
-namespace Tests\Feature;
+    namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Http;
-use Tests\TestCase;
+    use Illuminate\Foundation\Testing\RefreshDatabase;
+    use Illuminate\Foundation\Testing\WithFaker;
+    use Illuminate\Support\Facades\Http;
+    use Tests\TestCase;
 
-class ShortUrlApiTest extends TestCase
-{
-const CUSTOM_TOKEN = '[]{}()';
-
-    /** @test */
-    public function it_returns_a_shortened_url()
+    class ShortUrlApiTest extends TestCase
     {
+        const CUSTOM_TOKEN = '[]{}()';
 
-        $this->mockTinyUrlApi('http://www.example.com', 'https://tinyurl.com/example');
+        /** @test */
+        public function it_returns_a_shortened_url()
+        {
 
-        $response = $this->postJson('/api/v1/short-urls', [
-            'url' => 'http://www.example.com',
-        ], ['Authorization' => 'Bearer ' . self::CUSTOM_TOKEN]);
+            $this->mockTinyUrlApi('http://www.example.com', 'https://tinyurl.com/example');
 
-        // Verificar que la respuesta tenga el status correcto
-        $response->assertStatus(200)
-            ->assertJson([
-                'url' => 'https://tinyurl.com/example',
+            $response = $this->postJson('/api/v1/short-urls', [
+                'url' => 'http://www.example.com',
+            ], ['Authorization' => 'Bearer ' . self::CUSTOM_TOKEN]);
+
+            // Verificar que la respuesta tenga el status correcto
+            $response->assertStatus(200)
+                ->assertJson([
+                    'url' => 'https://tinyurl.com/example',
+                ]);
+        }
+
+        /** @test */
+        public function it_returns_error_when_url_is_missing()
+        {
+            $response = $this->postJson('/api/v1/short-urls', [], ['Authorization' => 'Bearer ' . self::CUSTOM_TOKEN]);
+
+            $response->assertStatus(422)
+                ->assertJsonValidationErrors('url');
+        }
+
+        /** @test */
+        public function it_returns_error_when_url_is_invalid()
+        {
+            $response = $this->postJson('/api/v1/short-urls', [
+                'url' => 'fake-url',
+            ], ['Authorization' => 'Bearer ' . self::CUSTOM_TOKEN]);
+
+            $response->assertStatus(422)
+                ->assertJsonValidationErrors('url');
+        }
+
+
+
+
+        protected function mockTinyUrlApi($url, $shortenedUrl)
+        {
+            Http::fake([
+                'https://api.tinyurl.com/create' => Http::response([
+                    'result' => $shortenedUrl,
+                ], 200),
             ]);
+        }
+
+
     }
-
-      /** @test */
-    public function it_returns_error_when_url_is_missing()
-    {
-        $response = $this->postJson('/api/v1/short-urls', [], ['Authorization' => 'Bearer ' . self::CUSTOM_TOKEN]);
-
-        $response->assertStatus(422)
-            ->assertJsonValidationErrors('url');
-    }
-
-
-    protected function mockTinyUrlApi($url, $shortenedUrl)
-    {
-        Http::fake([
-            'https://api.tinyurl.com/create' => Http::response([
-                'result' => $shortenedUrl,
-            ], 200),
-        ]);
-    }
-
-
-}
