64cfafd885ef005a42ba57db32d2752b77728818
Robert <robert.deabreu@borrox.com>
2024-11-26 23:42:26 +0100

    new controller

---
+++ app/Http/Controllers/ShortUrlController.php
@@ -0,0 +1,18 @@
+<?php
+
+    namespace App\Http\Controllers;
+
+
+    class ShortUrlController extends Controller
+    {
+
+
+        public function create()
+
+        {
+
+
+        }
+
+
+    }
